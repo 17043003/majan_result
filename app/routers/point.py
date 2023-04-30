d5bcd977c2347e5f782bbd608b6129789f6eaa24
17043003 <ishizuka533725@gmail.com>
2023-05-01 01:24:14 +0900

    add: calculate with open tiles and some yaku.

--- app/routers/point.py
+++ app/routers/point.py
@@ -9,25 +9,25 @@ router = APIRouter()
 async def detail_point(
     m: str = '', s: str = '', p: str = '', h: str = '',
     w_m: str = '', w_s: str = '', w_p: str = '', w_h: str = '',
-    dora_m: str = '', dora_s: str = '', dora_p: str = '', dora_h: str = '',
-    tsumo: bool = False, bakaze: str='00'):
+    opens: str = '',
+    own_wind: int = 0, field_wind: int = 0,
+    tsumo: bool = False, yakus: str = '000000', ):
 
     mp = majan_point.MajanPoint()
     hands = {'man': m, 'sou': s, 'pin': p, 'honors': h}
     win_hand = {'man': w_m, 'sou': w_s, 'pin': w_p, 'honors': w_h}
 
-    sum_count = 0
+    sum_count = 1
     for count in hands.values():
         sum_count += len(count)
+
+    melds = opens.split('_')
+    sum_count += len(melds) * 3
     
     if(sum_count < 14 ):
         raise HTTPException(status_code=404, detail="hands are lack.")
-    
-    dora_indicators = {'man': dora_m, 'sou': dora_s, 'pin': dora_p, 'honors': dora_h}
-    if all(not v for v in dora_indicators.values()):
-        dora_indicators = None
 
-    return mp.calc_point(hands, win_hand, dora_indicators, tsumo=tsumo)
+    return mp.calc_point(hands, win_hand, opens=melds, own_wind=own_wind, field_wind=field_wind, tsumo=tsumo, yakus=yakus)
 
 
 @router.get("/point/kokushi")
